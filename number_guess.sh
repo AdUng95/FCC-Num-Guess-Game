3e14a82de2806bca69d75735d542402e36be627d
ahhdumb <ungadam1995@gmail.com>
2023-01-30 23:31:28 +0000

    feat: add new user

--- number_guess.sh
+++ number_guess.sh
@@ -2,3 +2,13 @@
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
+echo "Enter your username:"
+read USERNAME
+
+AVAIL_USERNAME=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+
+if [[ -z $AVAIL_USERNAME ]]
+then
+  INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+  echo "Welcome, <username>! It looks like this is your first time here."
+fi
