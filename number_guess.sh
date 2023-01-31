dd287345d8d00a236542a63a9d5b487f17b3c2ed
ahhdumb <ungadam1995@gmail.com>
2023-01-31 22:45:07 +0000

    refactor: add spacing

--- number_guess.sh
+++ number_guess.sh
@@ -2,6 +2,8 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+echo -e "\n~~~ Number Guessing Game ~~~\n"
+
 echo "Enter your username:"
 read USERNAME
 
@@ -12,15 +14,15 @@ BEST_GAMES=$($PSQL "SELECT MIN(number_guesses) FROM users INNER JOIN games USING
 if [[ -z $AVAIL_USERNAME ]]
   then
   INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
-  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
   else
-  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAMES guesses."
+  echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAMES guesses."
 fi
 
 SECRET_NUMBER=$((1 + $RANDOM % 1000))
 TRIES=0
 GUESSED=0
-echo "Guess the secret number between 1 and 1000:"
+echo -e "\nGuess the secret number between 1 and 1000:"
 
 while [[ $GUESSED = 0 ]] 
 do
@@ -40,7 +42,7 @@ do
       elif [[ $GUESS -eq $SECRET_NUMBER ]]
         then
         TRIES=$(($TRIES + 1))
-        echo "You guessed it in $TRIES tries. The secret number was $SECRET_NUMBER. Nice job!"
+        echo -e "\nYou guessed it in $TRIES tries. The secret number was $SECRET_NUMBER. Nice job!\n"
         USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
         INSERT_GAMES=$($PSQL "INSERT INTO games(number_guesses, user_id) VALUES($TRIES, $USER_ID)")
         GUESSED=1
